d922237794bd90c6fcab01a7ab512a4cc98606da
c4p1t4n <leoigornunes@gmail.com>
2021-07-07 04:04:53 -0300

    Adicionando comentarios

--- Projetinho de metodos e Classes/Desafio Modulo classes e metodos/src/Comida.java
+++ Projetinho de metodos e Classes/Desafio Modulo classes e metodos/src/Comida.java
@@ -2,6 +2,8 @@
 public class Comida {
 	String nome;
 	double peso;
+	
+	// Constructor
 	Comida(String nome,double peso ) {
 		this.nome = nome;
 		this.peso = peso;
--- Projetinho de metodos e Classes/Desafio Modulo classes e metodos/src/Pessoa.java
+++ Projetinho de metodos e Classes/Desafio Modulo classes e metodos/src/Pessoa.java
@@ -2,14 +2,14 @@
 public class Pessoa {
 	String nome;
 	double peso;
-	
+	// Constructor
 	Pessoa(String nome,double peso){
 		this.nome = nome;
 		this.peso = peso;			
 	}
 	
 	
-	
+	// Metodo 
 	 void Comer(Comida comida) {			
 		System.out.println("Peso de "+ this.nome+ " antes da janta: "+ this.peso + "Kgs");
 		double pesoComida =  comida.peso;
