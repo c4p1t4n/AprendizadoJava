7d0b1f7b81c47e98f392a3740ba66f418fddf7c7
c4p1t4n <leoigornunes@gmail.com>
2021-07-19 20:38:14 -0300

    Adicionando MainProject

---
+++ OO/src/MainProject/CarrosMain.java
@@ -0,0 +1,19 @@
+package MainProject;
+
+import desafio_heranca.Opala;
+import desafio_heranca.Veyron;
+
+public class CarrosMain {
+
+	public static void main(String[] args) {
+		Veyron Bugatti = new Veyron();
+		Opala Opala = new Opala();
+		Bugatti.acelerar();
+		Opala.acelerar();
+		
+		
+		
+		
+	}
+
+}
